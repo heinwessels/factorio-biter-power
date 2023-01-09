d11cb4f47e8d35c9ae02fc3c949f66cb9123dace
Hein Wessels <heinwessels93@gmail.com>
2023-01-09 13:33:48 +0100

    unlock husbandry 1 during migration if player already had caged biters

--- migrations/0.3.0.lua
+++ migrations/0.3.0.lua
@@ -1,3 +1,13 @@
 for index, force in pairs(game.forces) do
     force.reset_technology_effects()
- end
+
+    -- If player had biter-power researched previously, and have 
+    -- produced or captured caged biters, then unlock first tech
+    if force.technologies["bp-biter-power"].researched then
+        local statistics = force.item_production_statistics
+        if statistics.get_input_count("bp-caged-small-biter") > 0 then
+            force.technologies["bp-biter-capture-tier-1"].researched = true
+        end
+    end
+end
+
